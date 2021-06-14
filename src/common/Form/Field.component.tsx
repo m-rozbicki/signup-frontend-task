909837586b10cbc1a8c611f91952f61cf84a9b95
m-rozbicki <rozbicki@protonmail.com>
2021-06-14 22:52:22 +0200

    Add aria-label to Field component

--- src/common/Form/Field.component.tsx
+++ src/common/Form/Field.component.tsx
@@ -1,13 +1,12 @@
-import React, { useState, InputHTMLAttributes } from 'react';
-import { v4 as uuid } from 'uuid';
+import React, { InputHTMLAttributes } from 'react';
 import { FieldInputProps } from 'formik';
 import styles from './Field.module.scss';
 
 interface FieldProps {
   label: string;
+  formikProps: FieldInputProps<string | number | readonly string[] | undefined>;
   touched?: boolean;
   error?: string;
-  formikProps?: FieldInputProps<string | number | readonly string[] | undefined>;
   inputProps?: InputHTMLAttributes<HTMLInputElement>;
 }
 
@@ -17,31 +16,32 @@ const Field = ({
   error = '',
   formikProps,
   inputProps = {},
-}: FieldProps) => {
-  const [fieldId] = useState(() => uuid());
-
-  return (
-    <div className={styles['field']}>
-      <div id={`form-error_${fieldId}`} className={styles['field__error']} role="alert">
-        {touched ? error : ''}
-      </div>
-      <label>
-        {label}
-        <span className={styles['field__required-asterisk']}>
-          {inputProps.required ? ' *' : ''}
-        </span>
-        <input
-          className={`${styles['field__input']} ${
-            touched && error ? styles['field__input--invalid'] : ''
-          }`}
-          aria-invalid={touched && error ? 'true' : 'false'}
-          aria-describedby={`form-error_${fieldId}`}
-          {...formikProps}
-          {...inputProps}
-        />
-      </label>
+}: FieldProps) => (
+  <div className={styles['field']}>
+    <div
+      id={`form-error_${formikProps.name}`}
+      className={styles['field__error']}
+      role="alert"
+      aria-label={`${label} error`}
+    >
+      {touched ? error : ''}
     </div>
-  );
-};
+    <label>
+      {label}
+      <span className={styles['field__required-asterisk']}>
+        {inputProps.required ? ' *' : ''}
+      </span>
+      <input
+        className={`${styles['field__input']} ${
+          touched && error ? styles['field__input--invalid'] : ''
+        }`}
+        aria-invalid={touched && error ? 'true' : 'false'}
+        aria-describedby={`form-error_${formikProps.name}`}
+        {...formikProps}
+        {...inputProps}
+      />
+    </label>
+  </div>
+);
 
 export default Field;
