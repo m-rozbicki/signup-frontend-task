b7f7009887667fe8d87556039c23dc8d56dea605
m-rozbicki <rozbicki@protonmail.com>
2021-06-15 20:47:51 +0200

    Add error prop to FormError

--- src/common/Form/FormError.component.tsx
+++ src/common/Form/FormError.component.tsx
@@ -4,11 +4,13 @@ import styles from './FormError.module.scss';
 interface FormErrorProps {
   isValid: boolean;
   touched: boolean;
+  error?: string | null;
 }
 
-const FormError = ({ touched, isValid }: FormErrorProps) => (
+const FormError = ({ touched, isValid, error }: FormErrorProps) => (
   <div className={styles['form-error']} aria-label="Form error" role="alert">
     {touched && !isValid && 'Form contains errors'}
+    {touched && isValid && error}
   </div>
 );
 
