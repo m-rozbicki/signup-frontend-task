db43c930204cfedf8397c123d5e1ce36dd19f6d1
m-rozbicki <rozbicki@protonmail.com>
2021-06-18 11:51:55 +0200

    Remove unnecessary import from Signup test

--- src/signup/Signup/Signup.test.tsx
+++ src/signup/Signup/Signup.test.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import {
-  act,
-  render, screen, waitFor,
-} from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Signup from './Signup';
 import { server } from '../../mocks/server';
