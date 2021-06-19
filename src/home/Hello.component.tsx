555b18dfcbf005193fd85f3af00d7d813c28ea38
m-rozbicki <rozbicki@protonmail.com>
2021-06-19 02:32:08 +0200

    Add Hello component

---
+++ src/home/Hello.component.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Submit } from '../common/Form';
+import Layout from '../common/Layout/Layout.component';
+import { useAuth } from '../services/Auth.context';
+import styles from './Hello.module.scss';
+
+const Hello = () => {
+  const { user, logout } = useAuth();
+  const [userName] = (user?.name ?? '').split(' ');
+
+  return (
+    <Layout title={`Hello, ${userName}`}>
+      <div className={styles['hello']}>
+        <div>
+          <div className={styles['hello__wave']}>👋</div>
+        </div>
+        <div>Thanks for checking in but you can only log out here.</div>
+        <div>
+          <Submit onClick={() => { logout(); }}>
+            Logout
+          </Submit>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export {
+  Hello,
+};
