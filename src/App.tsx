a5dc214ab7f4453b71b3d526784c9d4449be0bfc
m-rozbicki <rozbicki@protonmail.com>
2021-06-20 18:58:09 +0200

    Remove page transitions

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import React, { useRef } from 'react';
-import { HashRouter, useLocation } from 'react-router-dom';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import React from 'react';
+import { HashRouter } from 'react-router-dom';
 import { AuthProvider, useAuth } from './services/Auth.context';
 import { AuthenthicatedApp } from './AuthenthicatedApp';
 import { UnauthenthicatedApp } from './UnauthenthicatedApp';
-import transitions from './common/transitions.module.scss';
 
 const App = () => (
   <AuthProvider>
@@ -16,29 +14,15 @@ const App = () => (
 
 const TransitionSwitch = () => {
   const { user } = useAuth();
-  const location = useLocation();
-  const nodeRef = useRef(null);
 
   return (
-    <TransitionGroup>
-      <CSSTransition
-        key={location.pathname}
-        classNames={{
-          enter: transitions['fade--enter'],
-          enterActive: transitions['fade--enter-active'],
-        }}
-        timeout={350}
-        nodeRef={nodeRef}
-      >
-        <div ref={nodeRef} className={transitions['transition-container']}>
-          {
-            user
-              ? <AuthenthicatedApp location={location} />
-              : <UnauthenthicatedApp location={location} />
-          }
-        </div>
-      </CSSTransition>
-    </TransitionGroup>
+    <>
+      {
+        user
+          ? <AuthenthicatedApp />
+          : <UnauthenthicatedApp />
+      }
+    </>
   );
 };
 
